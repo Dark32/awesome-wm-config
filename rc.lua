6f48eb62a77bf1e700069a786320a43dad5d941a
Timur Kiyivinski <deathseclipse@gmail.com>
2015-06-08 09:09:11 +0800

    Added shortcuts

--- rc.lua
+++ rc.lua
@@ -103,7 +103,8 @@ awesomemenu = {
 
 settingsmenu = {
     { "audio", "pavucontrol"},
-    { "nvidia", "nvidia-settings"}
+    { "nvidia", "nvidia-settings"},
+    { "disks", "gksu gparted" }
 }
 
 systemmenu = {
@@ -325,20 +326,22 @@ function ()
 end),
 
 -- Standard program
-awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-awful.key({ modkey, "Control" }, "r", awesome.restart),
-awful.key({ modkey, "Shift"   }, "q", awesome.quit),
-awful.key({ modkey, "Shift"   }, "l", function () awful.util.spawn("i3lock -c 000000") end),
-awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
-awful.key({ modkey, "Control" }, "n", awful.client.restore),
-awful.key({ }, "Print", function () awful.util.spawn("scrot -e 'mv $f ~/Pictures/ 2>/dev/null'") end),
+awful.key({ modkey,           }, "Return",  function () awful.util.spawn(terminal) end),
+awful.key({ modkey, "Control" }, "r",       awesome.restart),
+awful.key({ modkey, "Shift"   }, "q",       awesome.quit),
+awful.key({ modkey,           }, "l",       function () awful.tag.incmwfact( 0.05)    end),
+awful.key({ modkey,           }, "h",       function () awful.tag.incmwfact(-0.05)    end),
+awful.key({ modkey, "Shift"   }, "h",       function () awful.tag.incnmaster( 1)      end),
+awful.key({ modkey, "Shift"   }, "l",       function () awful.tag.incnmaster(-1)      end),
+awful.key({ modkey, "Control" }, "h",       function () awful.tag.incncol( 1)         end),
+awful.key({ modkey, "Control" }, "l",       function () awful.tag.incncol(-1)         end),
+awful.key({ modkey,           }, "space",   function () awful.layout.inc(layouts,  1) end),
+awful.key({ modkey, "Shift"   }, "space",   function () awful.layout.inc(layouts, -1) end),
+awful.key({ modkey, "Control" }, "n",       awful.client.restore),
+awful.key({ }, "Print", function ()         awful.util.spawn("scrot -e 'mv $f ~/Pictures/ 2>/dev/null'") end),
+awful.key({ modkey, "Shift"   }, "l",       function () awful.util.spawn("i3lock -c 000000") end),
+awful.key({ modkey, "Shift"   }, "f",       function () awful.util.spawn("firefox") end),
+awful.key({ modkey, "Shift"   }, "n",       function () awful.util.spawn("nautilus") end),
 
 -- Prompt
 awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
